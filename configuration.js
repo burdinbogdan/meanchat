c338ca1faebae579924d000b8ffe9c07dcd1a767
burdinbogdan <burdinbogdan@gmail.com>
2017-09-05 03:08:09 +0300

    Delete configuration.js

--- configuration.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	PORT: process.env.PORT || 8080,
-	connectionDBString: process.env.mongoConDBStr || 'mongodb://admin:admin123@ds155191.mlab.com:55191/chatdb',
-	// connectionDBString: process.env.mongoConDBStr || 'mongodb://localhost/meanchatdb',
-	envStatus: process.env.status || 'dev',
-	secretTokkenWord: process.env.secretTokkenWord || 'secretword'
-}
